825b9fbad0315e3b0969cbc868dbe61370f616b4
Gopal-Swami <Gopalswami8619@gmail.com>
2022-01-03 02:51:53 -0800

    Version 0.2
    Header UI Setup Completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,19 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome To Proshop</h1>
+        </Container>
+      </main>
+
+      <Footer />
+    </>
+  );
 }
 
 export default App;
