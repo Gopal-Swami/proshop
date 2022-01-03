8d5373edd0d046d8bbf590a02265595dcacd5d77
Gopal-Swami <Gopalswami8619@gmail.com>
2022-01-03 11:45:01 -0800

    Version 0.3
    Added Backend server for creating API endpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,24 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome To Proshop</h1>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
 
       <Footer />
-    </>
+    </Router>
   );
 }
 
